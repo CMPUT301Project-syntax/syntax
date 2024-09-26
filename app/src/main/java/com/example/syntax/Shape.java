9ba3d23f093ed7814ef06231d50b88ef4a9dea6f
lubertoli <96629311+lubertoli@users.noreply.github.com>
2024-09-25 19:02:32 -0600

    saving shape

---
+++ app/src/main/java/com/example/syntax/Shape.java
@@ -0,0 +1,15 @@
+package com.example.syntax; // repo name
+
+// Lab 4 Shape Class
+public abstract class Shape {
+    int x;
+    int y;
+
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract void example();
+}
---
+++ app/src/main/java/com/example/syntax/Square.java
@@ -0,0 +1,9 @@
+package com.example.syntax;
+
+public class Square extends Shape {
+
+    public Square(int x) {
+        super(x, x);
+    }
+
+}
